7ec6cc1d6c3a5064f7704bfd404951d23e5a5015
Andreas Kovacs <ak225es@student.lnu.se>
2023-04-19 20:43:00 +0200

    fixed the call from before

--- app/src/main/java/controller/App.java
+++ app/src/main/java/controller/App.java
@@ -8,21 +8,15 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import view.LoginWindow;
 
 public class App extends Application {
 
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    //Group -> Scene -> Stage
-    //Stage stage = new Stage();
-    Group root = new Group();
-    Scene scene = new Scene(root, Color.BLACK);
-
-    primaryStage.setTitle("Login Page");
-
-    primaryStage.setScene(scene);
-    primaryStage.show();
+    LoginWindow loginWindow = new LoginWindow();
+    loginWindow.show();
   }
 
   public Connection dbconnect(){
--- app/src/main/java/view/LoginWindow.java
+++ app/src/main/java/view/LoginWindow.java
@@ -6,4 +6,15 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-//public class LoginWindow extends Application {  }
+public class LoginWindow extends Stage {
+    public LoginWindow(){
+        //Group -> Scene -> Stage
+        //Stage stage = new Stage();
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        setTitle("Login Page");
+
+        setScene(scene);
+    }
+}
