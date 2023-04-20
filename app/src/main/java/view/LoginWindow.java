3e5c72cd777fcf068c924e26cf785d80805be0b2
Andreas Kovacs <ak225es@student.lnu.se>
2023-04-20 21:27:51 +0200

    LoginWindow with scenebuilder

--- app/src/main/java/view/LoginWindow.java
+++ app/src/main/java/view/LoginWindow.java
@@ -1,20 +1,24 @@
 package view;
 
-import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+//import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class LoginWindow extends Stage {
     public LoginWindow(){
         //Group -> Scene -> Stage
         //Stage stage = new Stage();
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK);
-
-        setTitle("Login Page");
-
-        setScene(scene);
+        try {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("LoginWindow.fxml"));
+            //Group root = new Group();
+            Scene scene = new Scene(root);
+            setTitle("Login Page");
+            setScene(scene);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
