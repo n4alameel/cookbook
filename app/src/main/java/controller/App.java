d8727b3e4de2ec9b4d66c7dd2e91e715d2cef6fc
Manon Ykema <my222eu@student.lnu.se>
2023-04-25 11:47:00 +0200

    [US-01] Created a delay on the loader. Still basic.

--- app/src/main/java/controller/App.java
+++ app/src/main/java/controller/App.java
@@ -1,11 +1,13 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Preloader;
 import javafx.fxml.FXMLLoader;
@@ -14,56 +16,87 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.w3c.dom.Text;
 
 public class App extends Application {
 
-  private Loader loader = new Loader();
+    private Loader loader = new Loader();
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    Loader preloader = new Loader();
-    preloader.start(new Stage());
 
-    VBox root = new VBox();
-    root.setPadding(new Insets(5));
-    Label title = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+//  @Override
+//  public void start(Stage primaryStage) throws Exception {
+//    Loader preloader = new Loader();
+//    preloader.start(new Stage());
+//    preloader.stage.toFront();
+//
+//    VBox root = new VBox();
+//    root.setPadding(new Insets(5));
+//
+//    Label title = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+//
 
-    /*Label mysql;
-    try {
-      Connection conn = DriverManager
-        .getConnection("jdbc:mysql://localhost/cookbook?user=root&password=ABC&useSSL=false");
-      mysql = new Label("Driver found and connected");
-      Statement stmt = conn.createStatement();
-      ResultSet rs = stmt.executeQuery("SELECT * FROM user WHERE id = 1");
-      rs.next();
-      System.out.println(rs.getString("username")); //Exemple of a select from the database.
-    } catch (SQLException e) {
-      mysql = new Label("An error has occurred: " + e.getMessage());
-    }*/
+    //
+//    primaryStage.setScene(new Scene(root, 400, 200));
+//    primaryStage.setTitle("JavaFX");
+//    primaryStage.show();
+//    primaryStage.toBack();
+//
+//
+//    PauseTransition delay = new PauseTransition(Duration.seconds(1));
+//    delay.setOnFinished(event -> {
+//      primaryStage.show(); // Show the primary stage after the delay
+//      loader.stage.hide(); // Hide the loader
+//    });
+//  }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        loader.start(new Stage());
 
-    root.getChildren().addAll(title);
+        // Scene for Window that needs to open after Loader.
+        VBox root = new VBox();
+        root.setPadding(new Insets(5));
+        Label title = new Label("Lorem ipsum id est laborum.");
 
-    primaryStage.setScene(new Scene(root, 400, 200));
-    primaryStage.setTitle("JavaFX");
-    primaryStage.show();
+        /*Label mysql;
+        try {
+            Connection conn = DriverManager
+            .getConnection("jdbc:mysql://localhost/cookbook?user=root&password=ABC&useSSL=false");
+            mysql = new Label("Driver found and connected");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM user WHERE id = 1");
+            rs.next();
+            System.out.println(rs.getString("username")); //Exemple of a select from the database.
+            } catch (SQLException e) {
+                mysql = new Label("An error has occurred: " + e.getMessage());
+            }
+        */
 
-    preloader.stage.hide();
-    preloader.notifyPreloader(new Preloader.ProgressNotification(1));
-  }
+        root.getChildren().addAll(title/*, mysql*/);
+        primaryStage.setScene(new Scene(root, 400, 200));
+        primaryStage.setTitle("JavaFX");
+        primaryStage.show();
+        primaryStage.toBack();
 
-  public Connection dbconnect(){
-    Connection conn = null;
-    try {
-      conn = DriverManager
-        .getConnection("jdbc:mysql://localhost/cookbook?user=root&password=ABC&useSSL=false");
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
+        PauseTransition delay = new PauseTransition(Duration.seconds(2));
+        delay.setOnFinished(event -> {
+            loader.stage.hide();
+            primaryStage.toFront();
+        });
+        delay.play();
     }
-    return conn;
-  }
 
-  public static void main(String[] args) {
-    launch();
-  }
+    public Connection dbconnect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=ABC&useSSL=false");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
--- app/src/main/java/controller/Loader.java
+++ app/src/main/java/controller/Loader.java
@@ -2,13 +2,24 @@ package controller;
 
 import javafx.application.Preloader;
 import javafx.scene.Scene;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import javafx.application.Preloader;
+import javafx.scene.Scene;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Loader extends Preloader {
-    ProgressBar bar;
-    Stage stage;
+    private ProgressBar bar;
+    protected Stage stage;
+    private long startTime;
 
     private Scene createPreloaderScene() {
         bar = new ProgressBar();
@@ -23,16 +34,32 @@ public class Loader extends Preloader {
         stage.setScene(createPreloaderScene());
         stage.show();
         stage.toFront();
+        startTime = System.currentTimeMillis();
     }
 
     @Override
     public void handleProgressNotification(ProgressNotification pn) {
+        // Slow down the progress bar by pausing for 50 milliseconds on each update
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         bar.setProgress(pn.getProgress());
     }
 
     @Override
     public void handleStateChangeNotification(StateChangeNotification evt) {
         if (evt.getType() == StateChangeNotification.Type.BEFORE_START) {
+            // Pause for an additional 500 milliseconds to ensure a minimum duration of 1 second
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            long remainingTime = Math.max(0, 1000 - elapsedTime);
+            try {
+                Thread.sleep(remainingTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // Hide the preloader
             stage.hide();
         }
     }
