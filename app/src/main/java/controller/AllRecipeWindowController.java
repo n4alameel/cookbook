5b37350e318409f11c79d4a835360e04f03279cc
Andreas Kovacs <ak225es@student.lnu.se>
2023-04-28 00:33:46 +0200

    small tweaks

--- app/src/main/java/controller/AllRecipeWindowController.java
+++ app/src/main/java/controller/AllRecipeWindowController.java
@@ -6,13 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
-public class HomeWindowController {
-    public void openAllRecipes(javafx.event.ActionEvent event) throws IOException {
-        //TODO: wrong fxml for now need AllRecipe.FXML
-        Parent root = FXMLLoader.load(getClass().getResource("/AllRecipesWindow.fxml"));
+public class AllRecipeWindowController {
+    public void openHomepage(javafx.event.ActionEvent event) throws IOException {
+        //TODO: change windows and all
+        Parent root = FXMLLoader.load(getClass().getResource("/HomeWindow.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
