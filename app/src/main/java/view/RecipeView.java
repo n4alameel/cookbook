a31b08a82873b54818e1511f371055c3567f3ab2
Andreas Kovacs <ak225es@student.lnu.se>
2023-04-27 14:18:21 +0200

    running window.

---
+++ app/src/main/java/view/RecipeView.java
@@ -0,0 +1,15 @@
+package view;
+
+import controller.Controller;
+import javafx.scene.Parent;
+
+
+public class RecipeView {
+
+    private Parent root;
+    public RecipeView(Controller controller, ActiveView activeView) {
+    }
+    public Parent getRoot(){
+        return this.root;
+    }
+}
