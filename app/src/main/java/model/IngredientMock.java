42a3f420d6ef7c0ed4d38ba14f2f6de86193fdb7
Andreas Kovacs <ak225es@student.lnu.se>
2023-05-04 20:23:53 +0200

    mocking adding ingredients to the tableview for now, working(need to implement if we want to delete ingredients otherwise fine)

---
+++ app/src/main/java/model/IngredientMock.java
@@ -0,0 +1,55 @@
+package model;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class IngredientMock {
+  public int getId() {
+    return id.get();
+  }
+
+  public SimpleIntegerProperty idProperty() {
+    return id;
+  }
+
+  public String getName() {
+    return name.get();
+  }
+
+  public SimpleStringProperty nameProperty() {
+    return name;
+  }
+
+  public String getQuantity() {
+    return quantity.get();
+  }
+
+  public SimpleStringProperty quantityProperty() {
+    return quantity;
+  }
+
+  public String getUnit_id() {
+    return unit_id.get();
+  }
+
+  public SimpleStringProperty unit_idProperty() {
+    return unit_id;
+  }
+
+  private SimpleIntegerProperty id;
+  public SimpleStringProperty name;
+  private SimpleStringProperty quantity;
+  private SimpleStringProperty unit_id;
+
+  public IngredientMock(int id, String name, String quantity, String unit_id) {
+    this.id = new SimpleIntegerProperty(id);
+    this.name = new SimpleStringProperty(name);
+    this.quantity = new SimpleStringProperty(quantity);
+    this.unit_id = new SimpleStringProperty(unit_id);
+  }
+  public IngredientMock(String name, String quantity, String unit_id) {
+    this.name = new SimpleStringProperty(name);
+    this.quantity = new SimpleStringProperty(quantity);
+    this.unit_id = new SimpleStringProperty(unit_id);
+  }
+}
