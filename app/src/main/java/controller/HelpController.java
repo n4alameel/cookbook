ce82aeb6dc5b1258a89233597ba5a71f4fa1627b
Vanessa Jäger <vj222ji@student.lnu.se>
2023-05-28 19:27:26 +0200

    some new pictures and some editing of help page

--- app/src/main/java/controller/HelpController.java
+++ app/src/main/java/controller/HelpController.java
@@ -2,10 +2,14 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Side;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 public class HelpController {
   Controller controller = Controller.getInstance();
@@ -25,22 +29,32 @@ public class HelpController {
             tab.getStyleClass().remove("mark-tabs");
         }
         if (search.length() > 2) {
+
             for (Tab tab : tabPane.getTabs()) {
 
-                Label label = (Label) tab.getContent().lookup(".label");
-                TextArea textArea = (TextArea) tab.getContent().lookup(".text-area");
+                Set<Node> textAreas =  tab.getContent().lookupAll(".text-area");
 
-                if (textArea != null && label != null && textArea.getText().toLowerCase().contains(search) || label.getText().toLowerCase().contains(search)) {
-                    tab.getStyleClass().add("mark-tabs");
-                } else {
-                    tab.getStyleClass().remove("mark-tabs");
+                for(Node node: textAreas) {
+                    if (node instanceof TextArea) {
+                        TextArea textArea = (TextArea) node;
+                        String text = textArea.getText().toLowerCase();
+                        if (text.contains(search)) {
+                            tab.getStyleClass().add("mark-tabs");
+                        }
+                        else {
+                            tab.getStyleClass().remove("mark-tabs");
+                        }
+                        }
+
                 }
 
+                }
             }
         }
-    }
 
-    public void initialize() {
+
+
+    public void initialize () {
         tf.textProperty().addListener((observable, oldValue, newValue) -> {
             search();
         });
